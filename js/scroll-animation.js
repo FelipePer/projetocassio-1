0296d9e47efb84fd26bb6f4232acb3a1d7b5e491
adriaansaantos <adriandossantos76@gmai.com>
2023-07-01 13:42:38 -0300

    script nao funfa nessa merda

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,7 +1,26 @@
-const-scrollAnima = document.querySelectorAll('[data-anima="scroll"]');
+
+const scrollAnima = document.querySelectorAll('[data-anima="scroll"]');
+
+const metadeWindow = window.innerHeight * 5.2;
+
+function animarScroll() {
+
+    const topoItem = scrollAnima.getBroundingClientReact().top;
+
+    const itemVisivel = topoItem - metadeWindow < 0;
+
+    if(itemVisivel) {
+        scrollAnima.classList.add('show-button');
+    } else{
+        scrollAnima.classList.remove('show-button');
+    };
+
+    console.log(topoItem)
+
+};
 
 
-scrollanima.ad
+window.addEventListener('scroll', animarScroll);
 
 
 
